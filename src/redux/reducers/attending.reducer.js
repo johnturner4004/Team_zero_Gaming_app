0d624062c94304bc6b6f687c2f9468070010e0d2
johnturner4004 <johnturner4004@gmail.com>
2021-06-11 13:55:29 -0400

    added test for attendingReducer

--- src/redux/reducers/attending.reducer.js
+++ src/redux/reducers/attending.reducer.js
@@ -2,7 +2,7 @@ const attendingReducer = (state = '', action) => {
   switch (action.type) {
     case 'SET_ATTENDING':
       return action.payload;
-    default: 
+    default:
       return state;
   }
 }
--- src/redux/reducers/attendingReducer.test.js
+++ src/redux/reducers/attendingReducer.test.js
@@ -9,4 +9,12 @@ describe('Testing attendingReducer...', () => {
     expect( returnedState ).toEqual( '' );
   });
   
+  let payload = { user_id: 1, username: 'zeroone4004'}
+  test('testing set attinding used when getting list of players for an event...', () => {
+    let action = { type: 'SET_ATTENDING', payload: payload};
+    let state = '';
+    let returnedState = attendingReducer(state, action);
+    expect( returnedState ).toEqual( payload );
+  });
+
 });
