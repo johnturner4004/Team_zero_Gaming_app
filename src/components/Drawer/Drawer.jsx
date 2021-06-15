0fb3e311a90861e2fbdb7bf96ac4a49bd0e37a91
johnturner4004 <johnturner4004@gmail.com>
2021-06-14 23:03:41 -0400

    add comments to the drawer file

--- src/components/Drawer/Drawer.jsx
+++ src/components/Drawer/Drawer.jsx
@@ -1,3 +1,19 @@
+/***********************************************************
+************************************************************
+**                                                        ** 
+**  This is the code for the menu in the hamburger        **
+**  button. I will do my best to explain all the code     **
+**  in my comments. However, it may be easiest to start   **
+**  reading the code from the bottom return and reference **
+**  the code above to see what each element does. Most    **
+**  of this code is taken straight from Material-UI's     **
+**  website.                                              **
+**                                                        **
+************************************************************
+***********************************************************/
+
+
+
 import React from 'react';
 import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
@@ -23,24 +39,31 @@ const useStyles = makeStyles({
   },
 });
 
+
+// This function creates a custom component to use for each link
 function ListItemLink(props) {
   const { icon, primary, to } = props;
 
+  // This makes the link itself
   const renderLink = React.useMemo(
     () => React.forwardRef((itemProps, ref) => <RouterLink to={to} ref={ref} {...itemProps} />),
     [to],
   );
-
+  // This puts the link in a ListItem as a button component
   return (
     <li>
       <ListItem button component={renderLink}>
+        {/* If there is an icon for the link this will render it in front of the text */}
         {icon ? <ListItemIcon>{icon}</ListItemIcon> : null}
+        {/* This is the text for the link */}
         <ListItemText primary={primary} />
       </ListItem>
     </li>
   );
 }
 
+// This sets the type for each argument passed into the ListItemLink component
+// constructed above
 ListItemLink.propTypes = {
   icon: PropTypes.element,
   primary: PropTypes.string.isRequired,
@@ -63,6 +86,7 @@ export default function Drawer() {
 
   const user = useSelector(store => store.user)
 
+  // This function toggles whether the drawer is displayed or not
   const toggleDrawer = (anchor, open) => (event) => {
     if (event && event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
       return;
@@ -71,11 +95,14 @@ export default function Drawer() {
     setState({ ...state, [anchor]: open });
   };
 
+  // This is the logs out the user then forwards them back to the homepage when
+  // "Logout" is clicked
   const handleLogout = () => {
     dispatch({type: 'LOGOUT'})
     history.push('/home')
   }
 
+  // This sets up the list of links that are rendered when the drawer is open
   const list = (anchor) => (
     <div
       className={clsx(classes.list, {
@@ -90,16 +117,20 @@ export default function Drawer() {
         <ListItemLink to="/upcoming" primary="Upcoming Events" />
         {user.username ?
         <>
+        {/* The following links are only displayed if the user is logged in */}
         <ListItemLink to="/add-event" primary="Add Event" />
         <ListItemLink to="/my-events" primary="My Events" />
         </>
         : ''}
         <ListItemLink to="/about" primary="About" />
       </List>
+      {/* This divider renders a dividing line between the page nagivation links and the user account links */}
       <Divider />
       {!user.username ?
+      // The following link only displays if a user is not logged in and the remaining links after the colon are rendered when a user is logged in
       <ListItemLink to="/login" primary="Login" /> :
       <>
+      {/* The profile link is currently disables and will be added in a future update */}
       {/* <ListItemLink to="/profile" primary="Profile" /> */}
       <ListItem>
       <ListItemText  primary="Log out" 
@@ -108,8 +139,13 @@ export default function Drawer() {
     </div>
   );
 
+  // This descrips the part of the display the drawer comes from. Top makes it look
+  // like the drawer is being pulled down from the top of the page
   const anchor='top'
 
+  // The return statement takes all the elements described above and puts them
+  // together with the MenuIcon. The menu icon is the button made of three lines that
+  // is frequently called a hamburger button
   return (
     <div>
         <React.Fragment key={anchor}>
